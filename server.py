56e33d6ec515647a9400c97a511b2b049d4aed93
wh1t3.rvy <shifteskiy@gmail.com>
2026-07-05 14:53:51 +0300

    Update server.py

--- server.py
+++ server.py
@@ -238,4 +238,5 @@ def handle_end_round_early(data):
         emit('game_started', to=code)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
